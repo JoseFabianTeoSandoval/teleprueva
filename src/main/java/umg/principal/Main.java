a7a5616a70b7f20da3f213646b8a18c67ec130e4
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-08-17 02:21:47 -0600

    1.0

---
+++ src/main/java/umg/principal/Main.java
@@ -0,0 +1,31 @@
+package umg.principal;
+
+import umg.principal.botTelegram.bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Crea una instancia de TelegramBotsApi
+        TelegramBotsApi botsApi;
+
+        try {
+            // Inicializa TelegramBotsApi con una sesión por defecto
+            botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+            // Registra tu bot
+            botsApi.registerBot(new bot());
+
+            System.out.println("Bot registrado y funcionando...");
+
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+            System.out.println("Error al registrar el bot: " + e.getMessage());
+        }
+
+    }
+
+}
